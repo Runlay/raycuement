856b7f6ff70390e1b738dd26f35050e9f90feaee
Runlay <48648552+Runlay@users.noreply.github.com>
2020-05-30 20:52:03 +0200

    0.3

--- app.js
+++ app.js
@@ -7,14 +7,21 @@ new fullpage("#fullpage", {
   onLeave: (origin, destination, direction) => {
     const section = destination.item;
     const title = section.querySelector("h1"); 
-    const subs = document.getElementsByClassName("shows");
+    const shows = document.getElementsByClassName("shows");
+    const show1 = document.getElementsByClassName("show1");
+    const show2 = document.getElementsByClassName("show2");
+    const show3 = document.getElementsByClassName("show3");
+    const show4 = document.getElementsByClassName("show4");
     const player =document.getElementsByClassName("player");
     const tl = new TimelineMax({ delay: 0.2 });
 
       
     tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 });
-    tl.fromTo(subs, 0.5, { x: "-1000", opacity: 0.2 }, { x: "0", opacity: 1 });
-   
+    tl.fromTo(shows, 0.5, { x: "-1000", opacity: 0.2 }, { x: "0", opacity: 1 });
+    tl.fromTo(show1, 0.5, { x: "-1000", opacity: 0.2 }, { x: "0", opacity: 1 });
+    tl.fromTo(show2, 0.5, { x: "-1000", opacity: 0.2 }, { x: "0", opacity: 1 });
+    tl.fromTo(show3, 0.5, { x: "-1000", opacity: 0.2 }, { x: "0", opacity: 1 });
+    tl.fromTo(show4, 0.5, { x: "-1000", opacity: 0.2 }, { x: "0", opacity: 1 });
     
       if (destination.index === 1) {
       const pics = document.querySelectorAll(".chair");
